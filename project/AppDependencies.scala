3015d31da8b8741fb1ebaaea3579dc01bbc73387
fenallen <allen.fensome@gmail.com>
2021-08-24 16:02:51 +0100

    BDOG-1471 Bump auth-client and play version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,19 +5,20 @@ import sbt._
 
 object AppDependencies {
 
+  val bootstrapVersion = "5.12.0"
+
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-27" % "3.0.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "0.25.0-play-27",
-    "uk.gov.hmrc"             %% "play-frontend-govuk"        % "0.53.0-play-27"
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % bootstrapVersion,
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "1.1.0-play-28"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "3.0.0" % Test,
-    "org.scalatest"           %% "scalatest"                % "3.1.2"                 % Test,
-    "org.jsoup"               %  "jsoup"                    % "1.10.2"                % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % bootstrapVersion % Test,
+    "org.scalatest"           %% "scalatest"                % "3.2.9"                 % Test,
+    "org.jsoup"               %  "jsoup"                    % "1.14.2"                % Test,
     "com.typesafe.play"       %% "play-test"                % current                 % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.35.10"               % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"                 % "test, it",
-    "org.mockito"            %% "mockito-scala"            % "1.10.6"                   % Test
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8"               % "test, it",
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"                 % "test, it",
+    "org.mockito"            %% "mockito-scala"            % "1.16.37"                   % Test
   )
 }
